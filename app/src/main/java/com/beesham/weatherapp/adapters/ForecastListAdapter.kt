ae091087dd626113c694143f93a0154b6db912b1
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-08-29 18:29:50 -0400

    Chore: update gradle | Add ForecastListAdapter

---
+++ app/src/main/java/com/beesham/weatherapp/adapters/ForecastListAdapter.kt
@@ -0,0 +1,27 @@
+package com.beesham.weatherapp.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by Beesham on 8/28/2017.
+ */
+
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
