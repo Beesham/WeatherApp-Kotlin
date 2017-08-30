ce90af4bacfeee7b2c3b5259c73da8859eac7bf9
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-08-30 18:12:00 -0400

    Add Forecast data class

---
+++ app/src/main/java/com/beesham/weatherapp/data/Forecast.kt
@@ -0,0 +1,9 @@
+package com.beesham.weatherapp.data
+
+import java.util.*
+
+/**
+ * Created by Beesham on 8/30/2017.
+ */
+
+data class Forecast(val date: Date, val temparature: Float, val details: String)
