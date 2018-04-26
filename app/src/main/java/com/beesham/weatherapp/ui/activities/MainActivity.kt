96dfac2031d0a63411b1b56d8d34f2866a5f8aed
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2018-04-26 10:24:26 -0400

    Load data with better UI UI in adapter

--- app/src/main/java/com/beesham/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/beesham/weatherapp/ui/activities/MainActivity.kt
@@ -7,34 +7,31 @@ import android.support.v7.widget.RecyclerView
 import com.beesham.weatherapp.R
 import com.beesham.weatherapp.data.ForecastRequest
 import com.beesham.weatherapp.domain.commands.RequestForecastCommand
+import com.beesham.weatherapp.domain.model.Forecast
 import com.beesham.weatherapp.ui.adapters.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById<RecyclerView>(R.id.forecastList) as RecyclerView
+        val forecastList = findViewById<RecyclerView>(R.id.forecastList)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,
+                        object : ForecastListAdapter.OnItemClickListener{
+                            override fun invoke(forecast: Forecast) {
+                                toast(forecast.date)
+                            }
+                        })
             }
         }
     }
--- app/src/main/java/com/beesham/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/beesham/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -1,28 +1,58 @@
 package com.beesham.weatherapp.ui.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.beesham.weatherapp.R
+import com.beesham.weatherapp.domain.model.Forecast
 import com.beesham.weatherapp.domain.model.ForecastList
+import com.beesham.weatherapp.ui.utils.ctx
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.find
 
 /**
  * Created by Beesham on 8/28/2017.
  */
 
-class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(private val weekForecast: ForecastList, private val itemClick: ForecastListAdapter.OnItemClickListener):
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast[position]) {
-            holder.textView.text = "$date - $description - $high/$low"
-        }
+        holder.bindForecast(weekForecast[position])
     }
 
     override fun getItemCount(): Int = weekForecast.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view, itemClick)
     }
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(view: View, private val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+        private val iconView = view.find<ImageView>(R.id.icon)
+        private val dateView = view.find<TextView>(R.id.date)
+        private val descriptionView = view.find<TextView>(R.id.description)
+        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
+        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                //Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
+                dateView.text = date
+                descriptionView.text = description
+                maxTemperatureView.text = "${high} \u00B0"
+                minTemperatureView.text = "${low} \u00B0"
+                itemView.setOnClickListener { itemClick(this)}
+
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        operator fun invoke(forecast: Forecast)
+    }
 }
