ae240b87ac098d75cc2b1e7b2e4fb90d53369b72
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2018-05-04 20:21:55 -0400

    Chore: bump gradle and kotlin, Fix: not creating sql table

--- app/src/main/java/com/beesham/weatherapp/data/db/ForecastDbHelper.kt
+++ app/src/main/java/com/beesham/weatherapp/data/db/ForecastDbHelper.kt
@@ -20,7 +20,7 @@ class ForecastDbHelper(): ManagedSQLiteOpenHelper(App.instance,
                 CityForecastTable.COUNTRY to TEXT)
 
         db.createTable(DayForecastTable.NAME, true,
-                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.ID to SqlType.create("INTEGER PRIMARY_KEY AUTO_INCREMENT"),
                 DayForecastTable.DATE to INTEGER,
                 DayForecastTable.DESCRIPTION to TEXT,
                 DayForecastTable.HIGH to INTEGER,
